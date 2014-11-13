49b1810e923b66d063978a8e56c335f40cb72210
Andreas Kluth <mail@andreaskluth.net>
2014-11-13 21:37:55 +0100

    Removed noise from sample code.

--- src/main/java/net/andreaskluth/WebMvcConfiguration.java
+++ src/main/java/net/andreaskluth/WebMvcConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.annotation.DeferredResultMethodReturnValueHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ModelAndViewMethodReturnValueHandler;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 @Configuration
@@ -24,29 +23,20 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
   public HandlerMethodReturnValueHandler completableFutureReturnValueHandler() {
     return new CompletableFutureReturnValueHandler();
   }
-  
-  @Bean
-  public HandlerMethodReturnValueHandler modelAndViewWithBenefitsReturnValueHandler() {
-    return new ModelAndViewWithBenefitsReturnValueHandler();
-  }
 
   @PostConstruct
   public void init() {
     final List<HandlerMethodReturnValueHandler> originalHandlers = new ArrayList<>(
         requestMappingHandlerAdapter.getReturnValueHandlers());
     
-    final int deferredPos = obtainDeferredValueHandlerPosition(originalHandlers, DeferredResultMethodReturnValueHandler.class);
+    final int deferredPos = obtainValueHandlerPosition(originalHandlers, DeferredResultMethodReturnValueHandler.class);
     // Add our handler directly after the deferred handler.
     originalHandlers.add(deferredPos + 1, completableFutureReturnValueHandler());
-    
-    final int modelAndViewPos = obtainDeferredValueHandlerPosition(originalHandlers, ModelAndViewMethodReturnValueHandler.class);
-    // Add our handler directly before the deferred handler.
-    originalHandlers.add(modelAndViewPos, modelAndViewWithBenefitsReturnValueHandler());
     
     requestMappingHandlerAdapter.setReturnValueHandlers(originalHandlers);
   }
 
-  private int obtainDeferredValueHandlerPosition(final List<HandlerMethodReturnValueHandler> originalHandlers, Class<?> handlerClass) {
+  private int obtainValueHandlerPosition(final List<HandlerMethodReturnValueHandler> originalHandlers, Class<?> handlerClass) {
     for (int i = 0; i < originalHandlers.size(); i++) {
       final HandlerMethodReturnValueHandler valueHandler = originalHandlers.get(i);
       if (handlerClass.isAssignableFrom(valueHandler.getClass())) {
--- src/main/java/net/andreaskluth/controllers/CompletableFutureContoller.java
+++ src/main/java/net/andreaskluth/controllers/CompletableFutureContoller.java
@@ -1,7 +1,10 @@
 package net.andreaskluth.controllers;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,20 +13,29 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class CompletableFutureContoller {
 
+  private static final Log LOG = LogFactory.getLog(CompletableFutureContoller.class);
+  
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public ModelAndView index() {
     return new ModelAndView("index", "message", "bye bye sync world");
   }
+  
+  @RequestMapping(value = "/sync", method = RequestMethod.GET)
+  public ModelAndView sync() {
+    return new ModelAndView("brokentemplate", "message", "bye bye sync world");
+  }
 
   @RequestMapping(value = "/async", method = RequestMethod.GET)
   public CompletableFuture<ModelAndView> indexAsync() {
-    CompletableFuture<ModelAndView> futureModelAndView = new CompletableFuture<>();
 
-    // It's already completed however it is about getting the pipeline to work
-    // with CompletableFuture.
-    futureModelAndView.complete(new ModelAndView("brokentemplate", "message", "hello async world"));
+    LOG.info("Servlet Thread Id = '" + Thread.currentThread().getName() + "'.");
 
-    return futureModelAndView;
+    CompletableFuture<ModelAndView> runAsync = CompletableFuture.supplyAsync(() -> {
+      LOG.info("Backend Thread Id = '" + Thread.currentThread().getName() + "'.");
+      return new ModelAndView("brokentemplate", "message", "hello async world");
+    }, ForkJoinPool.commonPool());
+
+    return runAsync;
   }
 
 }
