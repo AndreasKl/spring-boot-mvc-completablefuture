360e346d1e6909e4b4523665b259d864f4b5b1c6
Andreas Kluth <mail@andreaskluth.net>
2014-11-13 21:32:30 +0100

    Housekeeping.

--- src/main/java/net/andreaskluth/controllers/ErrorController.java
+++ src/main/java/net/andreaskluth/controllers/ErrorController.java
@@ -9,13 +9,11 @@ public class ErrorController implements org.springframework.boot.autoconfigure.w
 
   @Override
   public String getErrorPath() {
-    // TODO Auto-generated method stub
     return "/error";
   }
 
   @RequestMapping("/error")
   public ModelAndView error() {
-    System.out.println("Und alle so yeah.");
     return new ModelAndView("error");
   }
 
